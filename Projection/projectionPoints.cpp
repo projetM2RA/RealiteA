d9537b6abfe17780801d57b2005c40f16e41a4d5
Delgankar <aelir.delgankar@gmail.fr>
2015-11-17 14:44:28 +0100

    maj

--- Projection/projectionPoints.cpp
+++ Projection/projectionPoints.cpp
@@ -3,7 +3,7 @@
 #include <sstream>
 #include <fstream>
 
-#define NBRIMAGESCALIB	17
+#define NBRIMAGESCALIB	20
 #define COLCHESSBOARD	9
 #define ROWCHESSBOARD	6
 
@@ -14,12 +14,14 @@ int main()
 
 	cv::Mat rvecs, tvecs;
 	std::vector<cv::Point2f> imagePoints;
+	std::vector<cv::Point2f> chessCornersInit;
 	std::vector<cv::Point3f> objectPoints;
+	std::vector<double> distances;
 
-	// Création des points à projeter
-	for(int x=0 ; x<COLCHESSBOARD ; x++)
-		for(int y=0 ; y<ROWCHESSBOARD ; y++)
-			objectPoints.push_back(cv::Point3f(x*26,y*26,0));	
+	// CrÃ©ation des points Ã  projeter
+	for(int x = 0; x < COLCHESSBOARD; x++)
+		for(int y = 0; y < ROWCHESSBOARD; y++)
+			objectPoints.push_back(cv::Point3f(x * 26, y * 26, 0));	
 
 	cv::FileStorage fs("../rsc/intrinsicMatrix.yml", cv::FileStorage::READ);
 
@@ -43,12 +45,23 @@ int main()
 
 		fs.release();
 
+		bool patternfound = cv::findChessboardCorners(imCalib[i], cv::Size(ROWCHESSBOARD, COLCHESSBOARD), chessCornersInit);
+
+		if(patternfound)
+			cv::cornerSubPix(imCalib[i], chessCornersInit, cv::Size(5, 5), cv::Size(-1, -1), cv::TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
+
+
 		// Projection
 		cv::projectPoints(objectPoints, rvecs, tvecs, cameraMatrix, distCoeffs, imagePoints);
 
-		// Dessin des points projetés
-		for(int m=0 ; m<objectPoints.size() ; m++)
-			cv::circle(imCalib[i],cv::Point(imagePoints[m].x, imagePoints[m].y), 3, cv::Scalar(0,0,255), 1, 8, 0);
+		for(int i = 0; i < COLCHESSBOARD * ROWCHESSBOARD; i++)
+		{
+			double d = (chessCornersInit[i].y - imagePoints[i].y) / (chessCornersInit[i].x - imagePoints[i].x);
+		}
+
+		// Dessin des points projetÃ©s
+		for(int m = 0; m < objectPoints.size(); m++)
+			cv::circle(imCalib[i], cv::Point(imagePoints[m].x, imagePoints[m].y), 3, cv::Scalar(0,0,255), 1, 8, 0);
 		
 		cv::imshow("image", imCalib[i]);
 
